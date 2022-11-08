c6a54472fa53951d141e0740c73c2648dd646444
Luca Luzzi <luzziluca6@gmail.com>
2022-11-08 16:34:28 -0300

    primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import './mylabel.css'
+import { AllCaps } from '../stories/components/MyLabel.stories';
+
+
+export interface MyLabelProps {
+    /**
+    * this is the message to display in the label
+    */
+    label: string;
+    /**
+    * this is the label size
+    */
+    size: 'normal'|'h1'|'h2'|'h3';
+    /**
+    * these are the available colors, pick any of them
+    */
+    color?: 'primary' | 'secondary' | 'tertiary',
+    /**
+    * write TRUE if you want your text capitalized
+    */
+    allCaps?: boolean,
+    /**
+     * What background color to use
+     */
+    fontColor?: string
+}
+
+
+export const MyLabel = ({ 
+    label = 'No Label',
+    size = 'normal',
+    color = 'primary',
+    allCaps = false,
+    fontColor
+ }: MyLabelProps ) => {
+  return (
+    <span className={`label ${ size } text-${ color }`} style={{ color: fontColor }}>
+        { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
